6c9480e25d292f0239a2fbdc779d1f62dcde4beb
RFS-cmd <76849500+RFS-cmd@users.noreply.github.com>
2023-07-17 22:04:39 -0500

    Update Sh.lua

--- Sh.lua
+++ Sh.lua
@@ -35,7 +35,7 @@ function Library:CreateWindow(windowname,windowinfo)
     
     Frame.Parent = fu8rj82n
     Frame.BackgroundColor3 = Color3.fromRGB(206, 186, 181)
-    Frame.BorderColor3 = Color3.fromRGB(181, 186, 208)
+    Frame.BorderColor3 = Color3.fromRGB(208, 186, 181)
     Frame.BorderSizePixel = 0
     Frame.Position = UDim2.new(0.289808273, 0, 0.313227266, 0)
     Frame.Size = UDim2.new(0, 432, 0, 285)
